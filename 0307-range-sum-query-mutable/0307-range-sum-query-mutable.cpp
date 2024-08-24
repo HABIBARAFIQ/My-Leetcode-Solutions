0687fa18339010a34d8cccde4dce738f3602280a
HABIBARAFIQ <143699588+HABIBARAFIQ@users.noreply.github.com>
2024-08-25 03:46:40 +0600

    Time: 395 ms (27.44%), Space: 179.4 MB (41.51%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,66 +1,58 @@
 class NumArray {
 private:
-    vector<int> numscopy;
-    vector<int> prefixSums;
     vector<int> segment_tree;
     int n;
-    int size;
-    void calculate_prefix_sum(int n, vector<int>& nums) {
-        for (int i = 0; i < n; ++i) {
-            prefixSums[i + 1] = prefixSums[i] + nums[i];
-        }
-    }
-    void create_seg_tree(int index, int left, int right) {
-        if (left == right) {
-            segment_tree[index] = numscopy[left];
-            return;
-        }
-        int mid = (left + right) / 2;
-        create_seg_tree(2 * index + 1, left, mid);
-        create_seg_tree(2 * index + 2, mid + 1, right);
-        segment_tree[index] = segment_tree[2 * index + 1] + segment_tree[2 * index + 2];
-    }
-    void update_seg_tree(int index, int left, int right, int indx, int val) {
-        if (left == right) {
-            segment_tree[index] = val;
-            return;
-        }
-        int mid = (left + right) / 2;
-        if (indx <= mid)
-            update_seg_tree(2 * index + 1, left, mid, indx, val);
-        else
-            update_seg_tree(2 * index + 2, mid + 1, right, indx, val);
-        segment_tree[index] = segment_tree[2 * index + 1] + segment_tree[2 * index + 2];
-    }
 
 public:
     NumArray(vector<int>& nums) {
         n = nums.size();
-        if (n == 0) return;
-        size = (2 * n);
-        segment_tree.resize(4 * n, 0);
-        prefixSums.resize(n + 1, 0);
-        numscopy = nums;
-
-        calculate_prefix_sum(n, nums);
-        create_seg_tree(0, 0, n - 1);
+        segment_tree.resize(4 * n, 0); // Allocating enough space for the segment tree
+        buildSegmentTree(nums, segment_tree, 0, 0, n - 1);
     }
+
     void update(int index, int val) {
-        int diff = val - numscopy[index];
-        numscopy[index] = val;
-        update_seg_tree(0, 0, n - 1, index, val);
-        for (int i = index + 1; i <= n; ++i) {
-            prefixSums[i] += diff;
+        updateSegmentTree(segment_tree, 0, 0, n - 1, index, val);
+    }
+
+    int sumRange(int left, int right) {
+        return rangeSumQuery(segment_tree, 0, 0, n - 1, left, right);
+    }
+
+private:
+    void buildSegmentTree(vector<int>& nums, vector<int>& segment_tree, int index, int left, int right) {
+        if (left == right) {
+            segment_tree[index] = nums[left];
+        } else {
+            int mid = left + (right - left) / 2;
+            buildSegmentTree(nums, segment_tree, 2 * index + 1, left, mid);
+            buildSegmentTree(nums, segment_tree, 2 * index + 2, mid + 1, right);
+            segment_tree[index] = segment_tree[2 * index + 1] + segment_tree[2 * index + 2];
         }
     }
-    int sumRange(int left, int right) {
-        return prefixSums[right + 1] - prefixSums[left];
+
+    int rangeSumQuery(vector<int>& segment_tree, int index, int left, int right, int L, int R) {
+        if (L <= left && right <= R) {
+            return segment_tree[index];
+        }
+        if (right < L || left > R) {
+            return 0;
+        }
+        int mid = left + (right - left) / 2;
+        return rangeSumQuery(segment_tree, 2 * index + 1, left, mid, L, R) +
+               rangeSumQuery(segment_tree, 2 * index + 2, mid + 1, right, L, R);
+    }
+
+    void updateSegmentTree(vector<int>& segment_tree, int index, int left, int right, int idx, int val) {
+        if (left == right) {
+            segment_tree[index] = val;
+        } else {
+            int mid = left + (right - left) / 2;
+            if (idx <= mid) {
+                updateSegmentTree(segment_tree, 2 * index + 1, left, mid, idx, val);
+            } else {
+                updateSegmentTree(segment_tree, 2 * index + 2, mid + 1, right, idx, val);
+            }
+            segment_tree[index] = segment_tree[2 * index + 1] + segment_tree[2 * index + 2];
+        }
     }
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * obj->update(index,val);
- * int param_2 = obj->sumRange(left,right);
- */
