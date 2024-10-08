bbafeefda3043ba5ef16f02f82a4564d28e060b6
HABIBARAFIQ <143699588+HABIBARAFIQ@users.noreply.github.com>
2024-10-08 14:27:54 +0600

    Time: 9 ms (54.45%), Space: 9.7 MB (36.2%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maxlength=0;
-        int len=0;
-        int right=0;
-        int left=0;
-        unordered_map<char,int>lpc;
-        int maxFreq = 0; 
-        int conv=0;;
+        int maxlength = 0;
+        int len = 0;
+        int right = 0;
+        int left = 0;
+        unordered_map<char, int> lpc;
+        int maxFreq = 0;
+        int conv = 0;
+        // If the window contains n characters, and the most frequent character
+        // appears maxFreq times, then   the other n - maxFreq characters would
+        // need to be replaced to match the most frequent character.
         while(right<s.length()){
-            lpc[s[right]]++;
-            maxFreq=max(lpc[s[right]],maxFreq);
-            if(right - left + 1 - maxFreq>k)
-            {
-                lpc[s[left]]--;
-                left++;
-            }
-            len=right-left+1;
-            maxlength=max(maxlength,len);
-            right++;
+        lpc[s[right]]++;
+        maxFreq = max(lpc[s[right]], maxFreq);
+        if (right - left + 1 - maxFreq > k) {
+            lpc[s[left]]--;
+            left++;
         }
-        return maxlength;
+        len = right - left + 1;
+        maxlength = max(maxlength, len);
+        right++;
     }
-};
+    return maxlength;
+}
+}
+;
