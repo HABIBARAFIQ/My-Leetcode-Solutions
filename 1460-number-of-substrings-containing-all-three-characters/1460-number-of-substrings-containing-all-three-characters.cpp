5eb40f023f43d90004923511a672719ac8a7428d
HABIBARAFIQ <143699588+HABIBARAFIQ@users.noreply.github.com>
2024-10-09 12:36:17 +0600

    Time: 17 ms (60.76%), Space: 10 MB (72.04%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -5,13 +5,23 @@ public:
         int left=0;
         int right=0;
         int substrings=0;
+        // while(right<s.size()){
+        //     map[s[right]]++;
+        //     while(map['a'] > 0 && map['b'] > 0 && map['c'] > 0){
+        //         substrings+=(s.size()-right);
+        //         // if(map[s[left]]==0)map.erase(map[s[left]]);
+        //         map[s[left]]--;
+        //         left++;
+        //     }
+        //     right++;
+        // }
+        // can do more optimize
+        int lastseen[3]={-1,-1,-1};
         while(right<s.size()){
-            map[s[right]]++;
-            while(map['a'] > 0 && map['b'] > 0 && map['c'] > 0){
-                substrings+=(s.size()-right);
-                // if(map[s[left]]==0)map.erase(map[s[left]]);
-                map[s[left]]--;
-                left++;
+            lastseen[s[right] - 'a'] = right;
+            if (lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1) {
+                int min_last_seen = min(lastseen[0], min(lastseen[1], lastseen[2]));
+                substrings += (min_last_seen + 1);
             }
             right++;
         }
